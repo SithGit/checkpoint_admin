7d8bb30cde02bb32ce8d6c7241327a902a71802a
sith <sith12323@gmail.com>
2024-04-25 19:07:20 +0700

    Add date range utility functions in get-date.util.ts

--- src/app/utils/get-date.util.ts
+++ src/app/utils/get-date.util.ts
@@ -14,3 +14,38 @@ export function getTodayDateRange(): IDate {
         endDate
     };
 }
+
+export function getWeekDateRange(): IDate {
+    const date = new Date();
+    const startDate = `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`;
+    date.setDate(date.getDate() + 7);
+    const endDate = `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`;
+
+    return {
+        startDate,
+        endDate
+    };
+}
+
+export function getMonthDateRange(): IDate {
+    const date = new Date();
+    const startDate = `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-01`;
+    date.setMonth(date.getMonth() + 1);
+    const endDate = `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-01`;
+
+    return {
+        startDate,
+        endDate
+    };
+}
+
+export function getYearDateRange(): IDate {
+    const date = new Date();
+    const startDate = `${date.getFullYear()}-01-01`;
+    const endDate = `${date.getFullYear() + 1}-01-01`;
+
+    return {
+        startDate,
+        endDate
+    };
+}
