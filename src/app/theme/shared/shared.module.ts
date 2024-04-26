4314a5688a3ba88ee36e17f451037f5e2b2b88e1
sith <sith12323@gmail.com>
2024-04-26 18:46:23 +0700

    Refactor and remove unused code blocks

--- src/app/theme/shared/shared.module.ts
+++ src/app/theme/shared/shared.module.ts
@@ -4,9 +4,8 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 // project import
-import { BreadcrumbComponent } from './components/breadcrumb/breadcrumb.component';
+
 import { SpinnerComponent } from './components/spinner/spinner.component';
-import { CardComponent } from './components/card/card.component';
 
 // third party
 import { NgScrollbarModule } from 'ngx-scrollbar';
@@ -15,31 +14,18 @@ import { NgScrollbarModule } from 'ngx-scrollbar';
 import { NgbDropdownModule, NgbNavModule, NgbModule, NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    BreadcrumbComponent,
-    NgbDropdownModule,
-    NgbNavModule,
-    NgbModule,
-    NgbCollapseModule,
-    NgScrollbarModule,
-    CardComponent
-  ],
-  exports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    BreadcrumbComponent,
-    SpinnerComponent,
-    NgbModule,
-    NgbDropdownModule,
-    NgbNavModule,
-    NgbCollapseModule,
-    NgScrollbarModule,
-    CardComponent
-  ],
-  declarations: [SpinnerComponent]
+    imports: [CommonModule, FormsModule, ReactiveFormsModule, NgbDropdownModule, NgbNavModule, NgbModule, NgbCollapseModule, NgScrollbarModule],
+    exports: [
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        SpinnerComponent,
+        NgbModule,
+        NgbDropdownModule,
+        NgbNavModule,
+        NgbCollapseModule,
+        NgScrollbarModule
+    ],
+    declarations: [SpinnerComponent]
 })
 export class SharedModule {}
